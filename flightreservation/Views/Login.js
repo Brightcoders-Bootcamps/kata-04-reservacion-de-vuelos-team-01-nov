59cc476275467c1e7a1866fce89b481bc4a6aab4
vmagana3 <vmagana3@ucol.mx>
2021-01-23 14:56:44 -0600

    Firebase and Aunth Clean Instalation

--- flightreservation/Views/Login.js
+++ flightreservation/Views/Login.js
@@ -4,21 +4,6 @@ import {View,Image,Text,TextInput,TouchableOpacity,StyleSheet} from 'react-nativ
 
 import auth from '@react-native-firebase/auth';
 
-auth()
-  .signInAnonymously()
-  .then(() => {
-    console.log('User signed in anonymously');
-  })
-  .catch(error => {
-    if (error.code === 'auth/operation-not-allowed') {
-      console.log('Enable anonymous in your firebase console.');
-    }
-
-    console.error(error);
-  });
-
-
-
 const Login = ({navigation})=>
 {
   return(
